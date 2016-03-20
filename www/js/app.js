063cacaaa4876e6722f1dd24f75f0d1b6baf2598
Vjeko Babic <vjeko.babic@windeln.de>
2016-03-20 05:48:49 +0100

    Mobile app running on emulator

--- www/js/app.js
+++ www/js/app.js
@@ -1,20 +1,9 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('goofi', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
       cordova.plugins.Keyboard.disableScroll(true);
     }
     if(window.StatusBar) {
@@ -22,3 +11,35 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+  .controller('goofiCtrl', ['$scope', '$window', function($scope, $window){
+    $scope.ipCorrect = false;
+    $scope.wrongIP = false;
+
+    $scope.link = '#';
+    $scope.image ='img/goofy.jpeg';
+
+    $scope.setIP = function(ip){
+      $window.msf.remote('http://'+ip+':8001/api/v2/',function(err, service){
+        console.log(service);
+        $window.app = service.application('xAaLHANm6Q.app','com.goofi.myapp');
+        $window.app.connect({name: 'Goofi Mobile'}, function (err) {
+          if(err) {
+            console.log(err);
+            $scope.wrongIP = true;
+          }
+          else {
+            console.log('Goofi has landed!');
+            $scope.ipCorrect = true;
+          }
+          $scope.$apply();
+        });
+        $window.app.on('say', function(msg, fromClient){
+          console.log(fromClient.attributes.name + ' says, ', msg);
+          $scope.link = msg.lnk;
+          $scope.image = msg.img;
+          $scope.$apply();
+        });
+      });
+    }
+
+  }])
